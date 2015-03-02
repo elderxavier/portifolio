c7844e60f93843c93564e480bcf4989795950f65
Elder Xavier <eldersxavier@gmail.com>
2015-03-01 22:09:44 -0400

    idendificando div de slides

--- include/js/main.js
+++ include/js/main.js
@@ -77,10 +77,11 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'include/img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
+											{image : 'include/img/slider-images/image01.jpg', title : '<div class="slide-content">slide 1</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image02.jpg', title : '<div class="slide-content">slide 2</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image03.jpg', title : '<div class="slide-content">slide 3</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">slide 4</div>', thumb : '', url : ''},  
+											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">slide 5</div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
